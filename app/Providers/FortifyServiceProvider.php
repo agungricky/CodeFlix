d4b6fa446dafd3127c903e42147fdcb90e7d45a8
agungricky07.github.io <agungricky07@gmail.com>
2025-09-07 11:23:24 +0700

    07/09/2025

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -48,5 +48,13 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function () {
             return view('auth.register');
         });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
     }
 }
